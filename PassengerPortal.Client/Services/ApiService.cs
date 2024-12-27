ffa7e8a7969e959b2e96b8e83035fce909bf59f5
korneliarozpara <korneliarozpara@gmail.com>
2024-12-27 12:03:02 +0100

    add departure and arrival time to /search-connections

--- PassengerPortal.Client/Services/ApiService.cs
+++ PassengerPortal.Client/Services/ApiService.cs
@@ -1,4 +1,4 @@
-using System.Net.Http.Json;
+/*using System.Net.Http.Json;
 using PassengerPortal.Shared.Models; // Zakładam, że tam masz Connection, Station, itp.
 
 namespace PassengerPortal.Client.Services
@@ -23,6 +23,40 @@ namespace PassengerPortal.Client.Services
         {
             // Format daty w ISO 8601, np. 2024-12-28T09:00:00
             var departureTimeFormatted = departureTime.ToString("o");
+
+            var query = $"api/connections/search?startStationId={startStationId}&endStationId={endStationId}&departureTime={departureTimeFormatted}";
+
+            var response = await _httpClient.GetAsync(query);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<List<Connection>>();
+        }
+    }
+}*/
+using System.Net.Http.Json;
+using PassengerPortal.Shared.Models; // Zakładam, że tam masz Connection, Station, itp.
+
+namespace PassengerPortal.Client.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Station>> GetStationsAsync()
+        {
+            var response = await _httpClient.GetAsync("api/stations");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<List<Station>>();
+        }
+
+        public async Task<List<Connection>> SearchConnectionsAsync(int startStationId, int endStationId, DateTime departureTime)
+        {
+            // Format daty w ISO 8601, np. 2024-12-28T09:00:00Z
+            var departureTimeFormatted = departureTime.ToUniversalTime().ToString("o");
 
             var query = $"api/connections/search?startStationId={startStationId}&endStationId={endStationId}&departureTime={departureTimeFormatted}";
 
--- PassengerPortal.Client/obj/Debug/net8.0/PassengerPortal.Client.AssemblyInfo.cs
+++ PassengerPortal.Client/obj/Debug/net8.0/PassengerPortal.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PassengerPortal.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0edde4e568674a78dd6e78d303178427a2e95a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e933c36e9ad72c549c9bec135f583a36126eeab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PassengerPortal.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PassengerPortal.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
